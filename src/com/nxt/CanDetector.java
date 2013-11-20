5bebf41010dcbeac4ea59e3f578f69a1186e4163
rabidworm <selslurjake1993@gmail.com>
2013-11-20 15:22:22 -0700

    Update CanDetector.java

--- src/com/nxt/CanDetector.java
+++ src/com/nxt/CanDetector.java
@@ -1,5 +1,46 @@
 package com.nxt;
 
-public class CanDetector {
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
 
+public class CanDetector {
+UltrasonicSensor s;
+	TouchSensor  t;
+	/*
+	Initialize
+	Initialize SonicSensor
+	Initialize TouchSensor
+	*/
+	public CanSensor()
+	{
+		s = new UltrasonicSensor(SensorPort.S3);
+		t = new TouchSensor(SensorPort.S2);
+	}
+	
+
+	/*
+	isCanTargeted
+	Send a ping with SonicSensor
+	if distance from ping is less than the size of the circle,
+		Assume a can is in front of the robot
+	*/
+	public boolean isCanTargeted()
+	{
+		s.ping();
+		
+		return (s.getDistance() < 200);
+	}
+	
+	
+	/*
+	isTouchingCan
+	If the TouchSensor is pushed,
+		Assume the robot is touching a can
+	*/
+	public boolean isTouchingCan()
+	{
+		return t.isPressed();
+	}
 }
+
